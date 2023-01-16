0a08c8b5516a8139b81fa6e0af769dcd7582b7d6
Notbadtrips <105052342+Notbadtrips@users.noreply.github.com>
2023-01-16 15:01:22 +0100

    conversor ImageToPixel (java)

--- ImageToPixel/src/imagetopixel/ImageToPixel.java
+++ ImageToPixel/src/imagetopixel/ImageToPixel.java
@@ -16,20 +16,20 @@ import javax.imageio.ImageIO;
 
 public class ImageToPixel {
 
-private static String imagePath = "perro.jpeg";
-    private static    String outputPath = "ImageToPixel.txt";
+    private static String imagePath = "paternoster.bmp";
+    private static String outputPath = "ImageToPixel.txt";
 
     public static void main(String[] args) {
-        
+
         BufferedImage image = null;
-        
+
         try {
             image = ImageIO.read(new File(imagePath));
             int width = image.getWidth();
             int height = image.getHeight();
             FileWriter fw = new FileWriter(outputPath);
             int count = 0;
-            
+
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     int pixel = image.getRGB(x, y);
@@ -37,15 +37,14 @@ private static String imagePath = "perro.jpeg";
                     int green = (pixel >> 8) & 0xff;
                     int blue = pixel & 0xff;
                     String hexPixel = String.format("$%02X%02X%02X", blue, green, red);
-                    if(count % 8 == 0) {
+                    if (count % 8 == 0) {
                         fw.write("DC.L ");
                     }
                     fw.write(hexPixel);
                     count++;
-                    if(count % 8 == 0) {
+                    if (count % 8 == 0) {
                         fw.write("\n");
-                    }
-                    else if(count % 8 != 0 && x != width - 1 && y != height -1) {
+                    } else if (count % 8 != 0 && x != width - 1 && y != height - 1) {
                         fw.write(", ");
                     }
                 }
@@ -57,5 +56,3 @@ private static String imagePath = "perro.jpeg";
         }
     }
 }
-
-
